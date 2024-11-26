fd1644ae32e6a713d9c052b9fd8bd3a4dd75e376
manishkumarajiva <manishkumarajiva@gmail.com>
2024-11-27 00:06:40 +0530

    filter products

--- server/src/controllers/product.controller.js
+++ server/src/controllers/product.controller.js
@@ -3,29 +3,55 @@ const ProductModel = require('../models/product.model.js');
 // ------------------- PRODUCT CONTROLLERS ------------------ //
 
 exports.CreateProduct = async (req, res) => {
-    try {     
+    try {
         const createResponse = await ProductModel.create(req.body);
-        if(!createResponse) return res.status(200).json({ status : 401, message : 'Failed to Create' });
+        if (!createResponse) return res.status(200).json({ status: 401, message: 'Failed to Create' });
 
-        res.status(200).json({ status : 201, success : true, message : 'Successfully Created', response : createResponse });
+        res.status(200).json({ status: 201, success: true, message: 'Successfully Created', response: createResponse });
 
     } catch (error) {
-        res.status(500).json({ status : 500, message : error.message,  error : error.stack });
+        res.status(500).json({ status: 500, message: error.message, error: error.stack });
     }
 }
 
 
 
 exports.ReadProduct = async (req, res) => {
-    
     try {
-        const getResponse = await ProductModel.find({});
-        if(!getResponse) return res.status(200).json({ status : 401, message : 'Failed to Fetched' });
+        const productCount = await ProductModel.countDocuments();
+        const query = ProductModel.find();
 
-        res.status(200).json({ status : 200, success : true, message : 'Successfully Fetched', response : getResponse });
+        // filter by category
+        if (req.query.category) {
+            query = query.find({ category: req.query.category });
+        }
+
+        // filter by brand
+        if (req.query.brand) {
+            query = query.find({ brand: req.query.brand })
+        }
+
+        // sorting
+        if (req.query._sort && req.query._order) {
+            query = query.sort({ [req.query._sort]: req.query._order });
+        }
+
+
+        if (req.query._page && req.query._limit) {
+            const pageSize = req.query._limit;
+            const page = req.query._page;
+
+            query = query.skip(pageSize * (page - 1)).limit(pageSize)
+        }
+
+        const getResponse = await query.exec();
+
+        if (!getResponse) return res.status(200).json({ status: 401, message: 'Failed to Fetched' });
+
+        res.status(200).json({ status: 200, success: true, message: 'Successfully Fetched', response: getResponse });
 
     } catch (error) {
-        res.status(500).json({ status : 500, message : error.message,  error : error.stack });
+        res.status(500).json({ status: 500, message: error.message, error: error.stack });
     }
 }
 
@@ -34,13 +60,13 @@ exports.ReadProduct = async (req, res) => {
 exports.UpdateProduct = async (req, res) => {
     const id = req.params.id;
     try {
-        const updateResponse = await ProductModel.findByIdAndUpdate(id, req.body, {new : true});
-        if(!updateResponse) return res.status(200).json({ status : 401, message : 'Failed to Update' });
+        const updateResponse = await ProductModel.findByIdAndUpdate(id, req.body, { new: true });
+        if (!updateResponse) return res.status(200).json({ status: 401, message: 'Failed to Update' });
 
-        res.status(200).json({ status : 201, success : true, message : 'Successfully Updated', response : updateResponse });
+        res.status(200).json({ status: 201, success: true, message: 'Successfully Updated', response: updateResponse });
 
     } catch (error) {
-        res.status(500).json({ status : 500, message : error.message,  error : error.stack });
+        res.status(500).json({ status: 500, message: error.message, error: error.stack });
     }
 }
 
@@ -51,12 +77,12 @@ exports.DeleteProduct = async (req, res) => {
 
     try {
         const deleteResponse = await ProductModel.findByIdAndDelete(id);
-        if(!deleteResponse) return res.status(200).json({ status : 401, message : 'Failed to Delete' });
+        if (!deleteResponse) return res.status(200).json({ status: 401, message: 'Failed to Delete' });
 
-        res.status(200).json({ status : 201, success : true, message : 'Successfully Delete', response : deleteResponse });
+        res.status(200).json({ status: 201, success: true, message: 'Successfully Delete', response: deleteResponse });
 
     } catch (error) {
-        res.status(500).json({ status : 500, message : error.message,  error : error.stack });
+        res.status(500).json({ status: 500, message: error.message, error: error.stack });
     }
 }
 
