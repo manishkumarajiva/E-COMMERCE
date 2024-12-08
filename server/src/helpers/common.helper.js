92fb5c9e642a14375a51eb9ff237b2edb7133d32
manishkumarajiva <manishkumarajiva@gmail.com>
2024-12-08 18:38:59 +0530

    --

--- server/src/helpers/common.helper.js
+++ server/src/helpers/common.helper.js
@@ -1,3 +1,14 @@
 exports.sanitizeUser = user => ({id : user.id, role : user.role});
 
 exports.sanitizeLoggedInUser = user => ({id : user.id, name : user.name, addresses : user.addresses})
+
+
+
+exports.cookieExtractor = function(req) {
+    let token = null;
+    if (req && req.cookies) {
+        token = req.cookies['jwt'];
+    }
+    return token;
+};
+
